5e623b5a37bdf1b4bba28a6bbbbd962df80903a3
Chigozie Boniface <97526590+chigozieboniface@users.noreply.github.com>
2024-10-24 16:26:07 +0200

    add null_non_existing_fields.sql

---
+++ macros/null_non_existing_fields.sql
@@ -0,0 +1,8 @@
+{% macro add_custom_fields() %}
+  {%- if var('tiktok_non_existing_fields', None) is not none -%}
+    {%- set custom_fields = var('tiktok_non_existing_fields').split(',') -%}
+    {%- for field in custom_fields -%}
+      SAFE_CAST(NULL AS STRING) AS {{ field }}{{ "," if not loop.last }}
+    {%- endfor -%}
+  {%- endif -%}
+{% endmacro %}
