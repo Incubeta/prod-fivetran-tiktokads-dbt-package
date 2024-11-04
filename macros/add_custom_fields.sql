ed6152da45705b87356ca0451d2b91f83ce970d7
Chigozie Boniface <97526590+chigozieboniface@users.noreply.github.com>
2024-11-04 16:59:32 +0200

    updated add_custom_fields.sql

--- macros/add_custom_fields.sql
+++ macros/add_custom_fields.sql
@@ -2,6 +2,9 @@
   {%- if var('tiktok_custom_fields', None) is not none -%}
     {%- set custom_fields = var('tiktok_custom_fields').split(',') -%}
     {%- for field in custom_fields -%}
+      {%- if field == "objective_type" -%}
+      SAFE_CAST({{ field }} AS STRING) AS campaign_objective_type,
+      {%- endif -%}
       SAFE_CAST({{ field }} AS STRING) AS {{ field }},
     {%- endfor -%}
   {%- endif -%}
