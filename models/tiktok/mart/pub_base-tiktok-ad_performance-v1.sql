167d57149e8b0c41263a8ba1421ae3e118c3e325
Miguelo Sana <miguelo@incubeta.com>
2025-03-17 14:29:37 +0100

    Use microbatch for pub_base model

--- models/tiktok/mart/pub_base-tiktok-ad_performance-v1.sql
+++ models/tiktok/mart/pub_base-tiktok-ad_performance-v1.sql
@@ -4,9 +4,6 @@ with advertiser_info as (
 report as (
   select *
     from {{ref('int_ad_performance-v1')}}
-    {% if is_incremental() %}
-    WHERE day >= {{dbt_date.n_days_ago(var('days_ago', 1), date="day")}}
-    {%endif%}
   )
 SELECT 
         {{dbt_utils.generate_surrogate_key(["day", "report.ad_id", "advertiser_id", "campaign_id"])}} as unique_id,
